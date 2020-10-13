ebeb0043a40c9752c1eedb3bc4a6268a3619c1cb
trade16fx <roll26@yandex.ru>
2020-10-14 00:11:19 +0300

    Закончил страницу каталога

--- #src/js/libs.js
+++ #src/js/libs.js
@@ -1,4 +1,6 @@
 @@include('../../node_modules/jquery/dist/jquery.min.js')
 @@include('../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js')
-@@include('../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js')
+// @@include('../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js')
 @@include('../../node_modules/swiper/swiper-bundle.js')
+@@include('../../node_modules/jquery-ui-slider/jquery-ui.js')
+@@include('./jquery.ui.touch-punch.min.js')
--- #src/js/main.js
+++ #src/js/main.js
@@ -1,23 +1,56 @@
-
-
 @@include('../modules/main-slider/_slider.js')
 @@include('../modules/brend-slider/_slider.js')
 
 jQuery(document).ready(function($) {
 
-	@@include('./_burger-menu.js');
-	@@include('./_ancor.js')
+  @@include('./_burger-menu.js');
+  @@include('./_ancor.js')
 
-	 if ($('.hit-slider').length){
+  if ($('.hit-slider').length) {
     var hitSwiper = undefined;
-    allInitSwiper('.hit-slider', hitSwiper ,991); 
+    allInitSwiper('.hit-slider', hitSwiper, 991);
   }
-	if ($('.hit-slider').length){
+  if ($('.hit-slider').length) {
     var slsSwiper = undefined;
-    allInitSwiper('.sale-slider', slsSwiper ,991); 
-  }  
+    allInitSwiper('.sale-slider', slsSwiper, 991);
+  }
 
-  function allInitSwiper(objSlider,  varSwiper,  intWidth) {
+  $('.js-filter-title').click(function(event) {
+    $(this).parents('.filter').children('.filter__body').slideToggle(400);
+    $(this).toggleClass('active');
+  });
+
+  $('.filter-btn').click(function(event) {
+    $('.filter-block').slideToggle(400);
+  });
+
+  $('.filter__label').click(function(event) {   
+    let check = $(this).prev('.filter__check');
+    if (check.is(':checked')){
+      check.prop('checked', false);
+    }
+    else{
+      check.prop('checked', true);
+    }
+  });
+
+
+
+  $(".slider-range").slider({
+    range: true,
+    min: 0,
+    max: 5000,
+    values: [300, 3000],
+    slide: function(event, ui) {
+      $(".slider-price__min").text(ui.values[0] + " руб.");
+      $(".slider-price__max").text(ui.values[1] + " руб.");
+      // $('.slider-price__min').html(ui.values[ 0 ] + " руб.");
+    }
+  });
+  $(".slider-price__min").text($(".slider-range").slider("values", 0) + " руб.");
+  $(".slider-price__max").text($(".slider-range").slider("values", 1) + " руб.");
+
+  function allInitSwiper(objSlider, varSwiper, intWidth) {
 
     var screenWidth = $(window).outerWidth();
     var objNext = objSlider + '__arrow_next';
@@ -26,10 +59,10 @@ jQuery(document).ready(function($) {
 
     console.info(objNext);
 
-    if ( (screenWidth <= (intWidth)) && (varSwiper == undefined)) {
+    if ((screenWidth <= (intWidth)) && (varSwiper == undefined)) {
       varSwiper = new Swiper(objSlider, {
         slidesPerView: 'auto',
-        loop: true,    
+        loop: true,
         // centeredSlides: true,
         autoHeight: true,
         // spaceBetween: 15,
@@ -37,14 +70,14 @@ jQuery(document).ready(function($) {
           el: objPagination,
           clickable: true,
           // dynamicBullets: true,
-          renderBullet: function (index, className) {
-          	return '<span class="' + className + '"><span>' + (index + 1) + '</span></span>';
-        	},
+          renderBullet: function(index, className) {
+            return '<span class="' + className + '"><span>' + (index + 1) + '</span></span>';
+          },
         },
         navigation: {
-	       nextEl: objNext,
-	       prevEl: objPrev,
-	     },
+          nextEl: objNext,
+          prevEl: objPrev,
+        },
       });
     } else if ((screenWidth > intWidth) && (varSwiper != undefined)) {
       varSwiper.destroy();
@@ -53,12 +86,12 @@ jQuery(document).ready(function($) {
       $(objSlider + ' .swiper-slide').removeAttr('style');
     }
   }
-  
+
 
   $(window).resize(function() {
-    if ($('.hit-slider').length) allInitSwiper('.hit-slider', hitSwiper,991);
-    if ($('.sale-slider').length) allInitSwiper('.sale-slider', slsSwiper,991);
-   
+    if ($('.hit-slider').length) allInitSwiper('.hit-slider', hitSwiper, 991);
+    if ($('.sale-slider').length) allInitSwiper('.sale-slider', slsSwiper, 991);
+
   });
 
 
@@ -72,17 +105,22 @@ let btnDropdown = document.querySelector('.js-dropdown');
 let body = document.querySelector('.body');
 
 let showCatalog = () => pnlCatalog.classList.add('fixed');
-btnCatalog.addEventListener( "click" , showCatalog);
+btnCatalog.addEventListener("click", showCatalog);
 
 let closeCatalog = () => pnlCatalog.classList.remove('fixed');
-btnClose.addEventListener( "click" , closeCatalog);
+btnClose.addEventListener("click", closeCatalog);
 
-let activeCatalog = () => pnlCatalog.classList.toggle('active');
+// let activeCatalog = () => pnlCatalog.classList.toggle('active');
 
-const screenWidth  = window.screen.width;
+const screenWidth = window.screen.width;
 
-if (screenWidth < 992) {
-	btnDropdown.addEventListener( "click" , activeCatalog);
+// if (screenWidth < 992) {
+btnDropdown.addEventListener("click", activeCatalog);
+// }
+
+function activeCatalog() {
+  pnlCatalog.classList.toggle('active');
+  this.classList.toggle('active');
 }
 
 /* block quantity */
@@ -91,24 +129,24 @@ const btnsPlus = document.querySelectorAll(".js-plus");
 const btnsMinus = document.querySelectorAll(".js-minus");
 
 for (let btnPlus of btnsPlus) {
-  btnPlus.addEventListener('click', clickPlus );
+  btnPlus.addEventListener('click', clickPlus);
 }
 for (let btnMinus of btnsMinus) {
-  btnMinus.addEventListener('click', clickMinus );
+  btnMinus.addEventListener('click', clickMinus);
 }
 
 function clickPlus(event) {
   let quantVal = this.parentElement.querySelector('.quantity__inp').value;
-  quantVal ++;
+  quantVal++;
   this.parentElement.querySelector('.quantity__inp').value = quantVal;
 }
+
 function clickMinus(event) {
   let quantVal = this.parentElement.querySelector('.quantity__inp').value;
   if (quantVal > 1) {
-    quantVal --;
+    quantVal--;
     this.parentElement.querySelector('.quantity__inp').value = quantVal;
-  }  
+  }
 }
 
 /* eof block quantity */
-
